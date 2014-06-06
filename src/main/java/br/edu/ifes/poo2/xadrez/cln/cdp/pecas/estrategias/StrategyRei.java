fb00d7846467b0b16f984758949e85f93c0a35af
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2014-06-06 18:05:32 -0300

    Implementação do StrategyRei.

--- src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/pecas/estrategias/StrategyRei.java
+++ src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/pecas/estrategias/StrategyRei.java
@@ -1,17 +1,26 @@
 package br.edu.ifes.poo2.xadrez.cln.cdp.pecas.estrategias;
 
 import br.edu.ifes.poo2.xadrez.cln.cdp.Posicao;
+import br.edu.ifes.poo2.xadrez.cln.cdp.pecas.Cor;
 
 public class StrategyRei extends StrategyPecaImpl {
 
     @Override
     public boolean validarMovimento(Posicao posicaoAnterior, Posicao novaPosicao) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        setVariaveis(posicaoAnterior, novaPosicao);
+
+        return !novaPosicao.existePeca() && Math.abs(colunaAtual - colunaNova) <= 1 && Math.abs(linhaAtual - linhaNova) <= 1;
     }
 
     @Override
     public boolean validarMovimentoCaptura(Posicao posicaoAnterior, Posicao novaPosicao) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        setVariaveis(posicaoAnterior, novaPosicao);
+        Cor corAtual = posicaoAnterior.getPeca().getCor();
+
+        //Tem que existir alguma peça na nova posição, além de serem de cores diferentes.
+        return novaPosicao.existePeca() && novaPosicao.getPeca().getCor() != corAtual
+                //O movimento tem que ser o mesmo que o de validar movimento.
+                && Math.abs(colunaAtual - colunaNova) <= 1 && Math.abs(linhaAtual - linhaNova) <= 1;
     }
 
 }
