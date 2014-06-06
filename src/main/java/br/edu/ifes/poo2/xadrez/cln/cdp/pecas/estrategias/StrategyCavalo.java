dc1d113404865a0d6ada7912b89d0494910ca49d
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2014-06-06 18:55:08 -0300

    Implementação da classe StrategyCavalo
    	modificado: src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/pecas/estrategias/StrategyCavalo.java

--- src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/pecas/estrategias/StrategyCavalo.java
+++ src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/pecas/estrategias/StrategyCavalo.java
@@ -1,5 +1,31 @@
 package br.edu.ifes.poo2.xadrez.cln.cdp.pecas.estrategias;
 
-public class StrategyCavalo {
+import br.edu.ifes.poo2.xadrez.cln.cdp.Posicao;
+import br.edu.ifes.poo2.xadrez.cln.cdp.pecas.Cor;
+
+public class StrategyCavalo extends StrategyPecaImpl {
+
+    @Override
+    public boolean validarMovimento(Posicao posicaoAnterior, Posicao novaPosicao) {
+        super.setVariaveis(posicaoAnterior, novaPosicao);
+
+        if (!novaPosicao.existePeca()) {
+            return ((Math.abs(colunaAtual - colunaNova) == 2) && (Math.abs(linhaAtual - linhaNova) == 1))
+                    || ((Math.abs(colunaAtual - colunaNova) == 1) && (Math.abs(linhaAtual - linhaNova) == 2));
+        }
+        return false;
+    }
+
+    @Override
+    public boolean validarMovimentoCaptura(Posicao posicaoAnterior, Posicao novaPosicao) {
+        super.setVariaveis(posicaoAnterior, novaPosicao);
+        Cor corAtual = posicaoAnterior.getPeca().getCor();
+
+        if (novaPosicao.existePeca() && novaPosicao.getPeca().getCor() != corAtual) {
+            return ((Math.abs(colunaAtual - colunaNova) == 2) && (Math.abs(linhaAtual - linhaNova) == 1))
+                    || ((Math.abs(colunaAtual - colunaNova) == 1) && (Math.abs(linhaAtual - linhaNova) == 2));
+        }
+        return false;
+    }
 
 }
