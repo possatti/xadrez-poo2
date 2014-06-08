529aec34412cf0cf25127cff01f3debfac34074c
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2014-06-08 15:00:09 -0300

    Implementação da classe Partida.
    	modificado: src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/Partida.java

--- src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/Partida.java
+++ src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/Partida.java
@@ -3,6 +3,8 @@ package br.edu.ifes.poo2.xadrez.cln.cdp;
 import br.edu.ifes.poo2.xadrez.cln.cdp.pecas.Peca;
 import java.util.Map;
 import br.edu.ifes.poo2.xadrez.cln.cdp.tabuleiro.Tabuleiro;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,6 +17,8 @@ public class Partida {
 
     private Map<Jogador, List<Peca>> pecasCapturadas;
 
+    private Map<Jogador, Integer> pontuacao;
+
     private Tabuleiro tabuleiro;
 
     private Jogador jogadorBranco;
@@ -24,7 +28,28 @@ public class Partida {
     private TipoPartida tipoPartida;
 
     public Partida(Jogador jogadorBranco, Jogador jogadorPreto, Tabuleiro tabuleiro) {
+        this.jogadorBranco = jogadorBranco;
+        this.jogadorPreto = jogadorPreto;
+        this.tabuleiro = tabuleiro;
+        this.pecasCapturadas = new HashMap<>();
+        this.pontuacao = new HashMap<>();
+        this.pecasCapturadas.put(jogadorPreto, new ArrayList<Peca>());
+        this.pecasCapturadas.put(jogadorBranco, new ArrayList<Peca>());
+    }
 
+    public void addPecaCapturada(Peca peca) {
+        Jogador jogador;
+        switch (peca.getCor()) {
+            case BRANCO:
+                jogador = this.jogadorBranco;
+                break;
+            case PRETO:
+                jogador = this.jogadorPreto;
+                break;
+            default:
+                throw new RuntimeException();
+        }
+        pecasCapturadas.get(jogador).add(peca);
     }
 
 }
