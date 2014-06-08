a99d5c40eb0dffdf2d5115b5020cb7ad64d810de
Phillipe Lopes <phillipe.alcantara.fonseca@gmail.com>
2014-06-08 14:45:50 -0300

    Implementação da classe Jogador
    	modificado: src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/Jogador.java

--- src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/Jogador.java
+++ src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/Jogador.java
@@ -1,9 +1,14 @@
 package br.edu.ifes.poo2.xadrez.cln.cdp;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Jogador {
 
-	private String nome;
+    private String nome;
 
-	private int pontuacao;
+    private int pontuacao;
 
 }
