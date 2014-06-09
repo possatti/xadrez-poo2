4978588d21d3bb9cb92d218287b097af2b077435
Lucas Possatti <lucas_possatti@hotmail.com>
2014-06-09 17:03:35 -0300

    Métodos de coversão do tabuleiro e suas peças.

--- src/main/java/br/edu/ifes/poo2/xadrez/ciu/cci/Conversor.java
+++ src/main/java/br/edu/ifes/poo2/xadrez/ciu/cci/Conversor.java
@@ -5,6 +5,11 @@
  */
 package br.edu.ifes.poo2.xadrez.ciu.cci;
 
+import br.edu.ifes.poo2.xadrez.ciu.cih.TipoPecaGrafica;
+import br.edu.ifes.poo2.xadrez.cln.cdp.dto.PecaDTO;
+import br.edu.ifes.poo2.xadrez.cln.cdp.pecas.Cor;
+import br.edu.ifes.poo2.xadrez.cln.cdp.pecas.TipoPeca;
+
 /**
  * Possui métodos para conversão de dados que sejam úteis para fazer a
  * comunicação entre o controlador e a camada de gestão de tarefas.
@@ -61,4 +66,70 @@ public class Conversor {
         return strColuna + strLinha;
     }
 
+    /**
+     * Converte o tabuleiro da sua representação no modelo para a sua
+     * representação que é usada pelos combonentes gráficos.
+     *
+     * @param tabuleiroModelo Representação do tabuleiro segundo o modelo.
+     * @return
+     */
+    public static TipoPecaGrafica[][] converterTabuleiro(PecaDTO[][] tabuleiroModelo) {
+        // Inicia o novo tabuleiro.
+        TipoPecaGrafica[][] tabuleiroGrafico = new TipoPecaGrafica[8][8];
+
+        // É necessário rotaciona-los para fazer a conversão.
+        for (int linha = 0; linha < 8; linha++) {
+            for (int coluna = 0; coluna < 8; coluna++) {
+                PecaDTO pecaDTO = tabuleiroModelo[coluna][linha];
+                tabuleiroGrafico[linha][coluna] = converterPeca(pecaDTO);
+            }
+        }
+
+        return tabuleiroGrafico;
+    }
+
+    public static TipoPecaGrafica converterPeca(PecaDTO dto) {
+        // Se não há peça, devolvemos null.
+        if (dto == null) {
+            return null;
+        }
+        
+        // Pega os dados do DTO.
+        Cor corPeca = dto.getCor();
+        TipoPeca tipoPeca = dto.getTipoPeca();
+        TipoPecaGrafica pecaGrafica = null;
+
+        // Converte para a peça gráfica.
+        if (corPeca == Cor.BRANCO) {
+            if (tipoPeca == tipoPeca.TORRE) {
+                pecaGrafica = TipoPecaGrafica.TORRE_BRANCO;
+            } else if (tipoPeca == TipoPeca.CAVALO) {
+                pecaGrafica = TipoPecaGrafica.CAVALO_BRANCO;
+            } else if (tipoPeca == TipoPeca.BISPO) {
+                pecaGrafica = TipoPecaGrafica.BISPO_BRANCO;
+            } else if (tipoPeca == TipoPeca.RAINHA) {
+                pecaGrafica = TipoPecaGrafica.DAMA_BRANCO;
+            } else if (tipoPeca == TipoPeca.REI) {
+                pecaGrafica = TipoPecaGrafica.REI_BRANCO;
+            } else if (tipoPeca == TipoPeca.PEAO) {
+                pecaGrafica = TipoPecaGrafica.PEAO_BRANCO;
+            }
+        } else {
+            if (tipoPeca == TipoPeca.TORRE) {
+                pecaGrafica = TipoPecaGrafica.TORRE_PRETO;
+            } else if (tipoPeca == TipoPeca.CAVALO) {
+                pecaGrafica = TipoPecaGrafica.CAVALO_PRETO;
+            } else if (tipoPeca == TipoPeca.BISPO) {
+                pecaGrafica = TipoPecaGrafica.BISPO_PRETO;
+            } else if (tipoPeca == TipoPeca.RAINHA) {
+                pecaGrafica = TipoPecaGrafica.DAMA_PRETO;
+            } else if (tipoPeca == TipoPeca.REI) {
+                pecaGrafica = TipoPecaGrafica.REI_PRETO;
+            } else if (tipoPeca == TipoPeca.PEAO) {
+                pecaGrafica = TipoPecaGrafica.PEAO_PRETO;
+            }
+        }
+        
+        return pecaGrafica;
+    }
 }
