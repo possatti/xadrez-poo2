4237d22d08c3c1cbb523aaf7a8c200047425fed5
Lucas Possatti <lucas_possatti@hotmail.com>
2014-07-07 00:13:25 -0300

    Correção na Classe 'Jogador'
    A classe foi corrigida para ter campos para o email, bem como o
    apelido do jogador também. E o id da classe foi colocado para ser
    gerado automaticamente.

--- src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/Jogador.java
+++ src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/Jogador.java
@@ -1,7 +1,10 @@
 package br.edu.ifes.poo2.xadrez.cln.cdp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
@@ -10,16 +13,22 @@ import lombok.NoArgsConstructor;
 @Setter
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 public class Jogador {
 
     @Id
+    @GeneratedValue
     private Long id;
 
+    @Column(unique = true)
+    private String apelido;
+
+    @Column(unique = true)
     private String nome;
+
+    @Column(unique = true)
+    private String email;
 
     private int pontuacao;
 
-    public Jogador(String nome) {
-        this.nome = nome;
-    }
 }
