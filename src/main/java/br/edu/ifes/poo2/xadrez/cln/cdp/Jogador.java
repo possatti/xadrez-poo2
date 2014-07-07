7bf4a1ecc86386005e3afe5b1a36573540199c6d
Lucas Possatti <lucas_possatti@hotmail.com>
2014-07-06 23:43:20 -0300

    Configuração da classe 'Jogador'
    A classe 'jogador' foi configurada para usar as notações do JPA

--- src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/Jogador.java
+++ src/main/java/br/edu/ifes/poo2/xadrez/cln/cdp/Jogador.java
@@ -1,11 +1,19 @@
 package br.edu.ifes.poo2.xadrez.cln.cdp;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
 public class Jogador {
+
+    @Id
+    private Long id;
 
     private String nome;
 
